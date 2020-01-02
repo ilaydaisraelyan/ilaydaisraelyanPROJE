9553732b5f9ffe97eec6918677822fe49e35a782
ilaydaisraelyan <55581336+ilaydaisraelyan@users.noreply.github.com>
2020-01-02 13:50:02 +0300

    ekle duzenle penceresi eklendi

---
+++ ilaydaisraelyanproje/ekleduzenle.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace ilaydaisraelyanproje
+{
+    partial class ekleduzenle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Uno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UAd;
+        private System.Windows.Forms.Label Duzenlenen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox UAciklama;
+        private System.Windows.Forms.Button Tamam;
+        private System.Windows.Forms.Button Iptal;
+        private System.Windows.Forms.TextBox UFiyat;
+        private System.Windows.Forms.TextBox UAdet;
+    }
+}
